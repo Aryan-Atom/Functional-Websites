c745c6ffe286f745eecf7cfc50efb588abe715a3
AryanAtom <atomwolf.atomwolf@gmail.com>
2025-09-18 12:55:47 +0530

    Making Constructor object

--- JavaScript Practice/script.js
+++ JavaScript Practice/script.js
@@ -1,1 +1,20 @@
-console.log("Hello World !")
+function Book(name , author , date ,code){
+
+    if(!new.target){ // accessing the new property being used or not
+        console.error("New keyword not used. Accessing as function, not as a constructor")
+    }
+    this.name=name;
+    this.author=author;
+    this.date=date;
+    this.code=code;
+
+    this.bookDescription = function(){
+      console.log(
+        "The book Title is : " + name + "The author is : " + author + "The publish date is" + date + "the code is : " + code 
+      )
+    }
+}
+
+const book1 = new Book("Abc" , "Xyz"  , "12/01/2025" , "#CODR");
+
+console.log(book1.bookDescription())
