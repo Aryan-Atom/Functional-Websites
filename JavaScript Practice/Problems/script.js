c79efd2f8f927fe1b8d012b0d47242ac36695254
AryanAtom <atomwolf.atomwolf@gmail.com>
2025-09-28 01:45:26 +0530

    Tasks

---
+++ JavaScript Practice/Problems/script.js
@@ -0,0 +1,27 @@
+// // Task 1
+// let para = document.querySelector("p");
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function(){
+//     para.textContent =  "Starting..."
+// })
+
+
+
+// Task 2 - Swap image
+
+let img1 = document.querySelector("#img1");
+let img2 = document.querySelector("#img2");
+
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click" , function(){
+
+    let src1 = img1.src;
+    let src2 = img2.src;
+
+    img1.src = src2;
+    img2.src = src1;
+
+})
