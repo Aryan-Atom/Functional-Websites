b53a3d084cc4e2e4d56a76a9f2d5e34120b07019
AryanAtom <atomwolf.atomwolf@gmail.com>
2025-10-01 22:33:08 +0530

    changes

--- JavaScript Practice/Problems/script.js
+++ JavaScript Practice/Problems/script.js
@@ -119,15 +119,15 @@
 
 // Task 7 -> Progress Bar
 
-let prog = document.querySelector("#progress");
-let h3 = document.querySelector("h3");
+// let prog = document.querySelector("#progress");
+// let h3 = document.querySelector("h3");
 
-let count = 0;
-let int = setInterval(function () {
-  if (count === 100) {
-    h3.style.opacity= 1;
-    clearInterval(int);
-  }
-  count++;
-  prog.style.width = count + "%";
-}, 100);
+// let count = 0;
+// let int = setInterval(function () {
+//   if (count === 100) {
+//     h3.style.opacity= 1;
+//     clearInterval(int);
+//   }
+//   count++;
+//   prog.style.width = count + "%";
+// }, 100);
