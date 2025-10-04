36246046e89618380ebe67dc2f5d4e80ed97a129
AryanAtom <atomwolf.atomwolf@gmail.com>
2025-10-05 01:42:29 +0530

    changes

---
+++ JavaScript Practice/Problems/Complex Problem/script.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector("#header");
+let container = document.querySelector("#card-container");
+
+btn.addEventListener("click", () => {
+  fetch("https://randomuser.me/api/")
+    .then((res) => res.json())
+    .then((data) => {
+      let response = data?.results[0];
+      container.innerHTML += `<div class="card bg-zinc-800 w-80 h-50 rounded-lg text-white">
+          <div class="user py-3 mt-2 mx-3 text-xl border-b-2">${response?.name?.first}</div>
+          <div class="gender mt-3 mx-3 text-lg font-semibold opacity-50">
+            Gender : ${response?.gender}
+          </div>
+          <div class="desc mt-2 mx-3 opacity-75">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet dolor
+            ducimus neque sed ratione ullam.
+          </div>
+        </div>`;
+    });
+});
