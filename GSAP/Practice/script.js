7587f74861d7e7121c9fa9b8d4f37b763689b8d5
AryanAtom <atomwolf.atomwolf@gmail.com>
2025-10-10 23:58:07 +0530

    changes

--- GSAP/Practice/script.js
+++ GSAP/Practice/script.js
@@ -1,5 +1,22 @@
-gsap.to("#box" , {
-    x:1000,
-    duration:2,
-    delay:1
-})
+// gsap.to("#box" , {
+//     x:1000,
+//     rotate:360,
+//     duration:2,
+//     backgroundColor:"blue",
+//     delay:1
+// })
+
+//Initial -> Final = to
+
+// gsap.from("#box" , {
+//     x:1000,
+//     rotate:360,
+//     duration:2,
+//     backgroundColor:"blue",
+//     delay:1
+// })
+
+//Final -> Initial from
+
+
+ const tl =  gsap.timeline();
