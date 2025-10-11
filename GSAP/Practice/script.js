47d3dfcfc347ed0776080374220bcb5b9020995c
AryanAtom <atomwolf.atomwolf@gmail.com>
2025-10-11 20:29:55 +0530

    changes

--- GSAP/Practice/script.js
+++ GSAP/Practice/script.js
@@ -19,4 +19,25 @@
 //Final -> Initial from
 
 
- const tl =  gsap.timeline();
+
+// Use of Timeline
+
+//  const tl =  gsap.timeline();
+
+//  tl.to("#box1" ,{
+//     x:1000,
+//     scale:2,
+//     duration:3,
+//     rotate:360
+//  })
+
+//   tl.to("#box2" ,{
+//     x:800,
+//     scale:2,
+//     duration:3,
+//  })
+//   tl.to("#box3" ,{
+//     x:900,
+//     scale:2,
+//     duration:3,
+//  })
