53923d4fe1a62ae2125ca8bef71467ef169f215b
AryanAtom <atomwolf.atomwolf@gmail.com>
2025-10-13 23:15:53 +0530

    changes

--- Locomotive/script.js
+++ Locomotive/script.js
@@ -1,7 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
