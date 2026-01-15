4b79cb302ae9322e2bb75363554311ff9fe2e914
AryanAtom <atomwolf.atomwolf@gmail.com>
2026-01-15 16:09:16 +0530

    changes

--- Threejs/script.js
+++ Threejs/script.js
@@ -1,10 +1,6 @@
 // ------------- Scene -------------
 
-
-
 const scene = new THREE.Scene();
-
-
 
 // --------------- Camera --------------
 
@@ -15,21 +11,19 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-
 // -------------------- Geometry / Object -----------------
 
 const geometry = new THREE.CylinderGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: "yellow" });
+const material = new THREE.MeshBasicMaterial({
+  color: "yellow",
+  wireframe: true,
+});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-
 // Camera Position (Else both camera and object origin will be at (0,0,0) and nothing will render)
 
-
 camera.position.z = 5;
-
-
 
 // ----------------- Setting up the renderer --------------------
 
@@ -38,16 +32,22 @@ const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 // document.body.appendChild(renderer.domElement);
 
-
-
-
-
 // Animating Object and rendering it using request animation
 
-function animate() {
-  renderer.render(scene, camera);
+// function animate() {
+//   renderer.render(scene, camera);
+//   cube.rotation.x += 0.1;
+//   cube.rotation.y += 0.1;
+//   cube.rotation.z += 0.1;
+// }
+// renderer.setAnimationLoop(animate);
+
+const animate = () => {
+  window.requestAnimationFrame(animate);
   cube.rotation.x += 0.1;
   cube.rotation.y += 0.1;
   cube.rotation.z += 0.1;
-}
-renderer.setAnimationLoop(animate);
+  renderer.render(scene, camera);
+};
+
+animate();
